3c0ff3a31490358bb655bfb177b51f4530c1931f
Brener <brener.leite@edu.unirio.br>
2021-02-27 21:22:09 -0300

    menu done, still without links

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,26 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlayCircle, faFilm, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faPlayCircle, faFilm, faTimes, faStar, faFireAlt, faForward } from '@fortawesome/free-solid-svg-icons';
 
-import Repository from '../../services/repository';
+import { fetchGenres } from '../../store/actions/genres';
 import { MenuStyle, Container, List, Icon, ListTitle, Close } from './style';
 
-const Menu = ({ isActive, setActive }) => {
-    const api = new Repository();
-    const [useGenres, setGenres] = useState([]);
-
-    const getGenres = async() => {
-        const {data} = await api.getGenres();
-
-        return data;
-    }
+const Menu = ({ 
+    isActive, 
+    setActive,
+    genres,
+    fetchGenres
+}) => {
 
     useEffect(() => {
-        getGenres().then((response) => {
-            setGenres(response.genres);
-        });
+        fetchGenres();
     }, []);
+
+    useEffect(() => {
+        console.log(genres)
+    }, [genres])
 
     return (
         <MenuStyle isActive={isActive}>
@@ -33,10 +32,29 @@ const Menu = ({ isActive, setActive }) => {
                 </Icon>
                 <div>
                     <div>
+                        <ListTitle>Discover</ListTitle>
+                    </div>
+                    <List>
+                        <li>
+                            <FontAwesomeIcon icon={faFireAlt} size="1x" style={{color: '#fff'}} />
+                            <a>Popular</a>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faStar} size="1x" style={{color: '#fff'}} />
+                            <a>Top rated</a>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faForward} size="1x" style={{color: '#fff'}} />
+                            <a>Upcoming</a>
+                        </li>
+                    </List>
+                </div>
+                <div>
+                    <div>
                         <ListTitle>Genres</ListTitle>
                     </div>
                     <List>
-                        {useGenres.map((genre, index) => {
+                        {genres.data.map((genre, index) => {
                             return (
                                 <li key={index}>
                                     <FontAwesomeIcon icon={faFilm} size="1x" style={{color: '#fff'}} />
@@ -59,7 +77,9 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-
+        fetchGenres: () => {
+            dispatch(fetchGenres())
+        }
     }
 }
 
